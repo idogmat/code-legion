00860b9bcd4f45123138e4089074e4593f92f60f
Dmitry <00912009@rambler.ru>
2022-07-07 16:09:04 +0300

    first draft

--- src/main/java/com/codelegion/Code/Legion/controllers/lernMapController/LernMapController.java
+++ src/main/java/com/codelegion/Code/Legion/controllers/lernMapController/LernMapController.java
@@ -1,4 +1,24 @@
 package com.codelegion.Code.Legion.controllers.lernMapController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LernMapController {
+    @GetMapping("/learn")
+    public String learn() {
+        String result;
+        String tokenAvtorization = "NOTNULL"; //
+        if (tokenAvtorization == null) {
+            result = "login";
+        } else {
+            //todo полученеие данных из бд учебных материалов
+            result = "learn";
+        }
+        return result;
+    }
+
+    //todo получение массива на материалы
+    //todo получение массива инструкций
+    //todo получение массива на лекции
 }
