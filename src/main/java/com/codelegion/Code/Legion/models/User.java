6eefd1961b9faebd341766cb2483d9272a19be38
kapich1192 <kapich1192@yandex.ru>
2022-07-07 13:28:23 +0300

    developing models

---
+++ src/main/java/com/codelegion/Code/Legion/models/User.java
@@ -0,0 +1,23 @@
+package com.codelegion.Code.Legion.models;
+
+public class User {
+    /*id*/
+    private Long id;
+    /*registration fields*/
+    private String name;
+    private String lastname;
+    private String tel;
+    private String email;
+    private String nick;
+    private String login;
+    private String password;
+    /*game part fields*/
+    private int level;
+    /*basic skills*/
+    private int htmlLevel;
+    private int contentLevel;
+    private int seoLevel;
+    private int cssLevel;
+    private int cLevel;
+    /**/
+}
