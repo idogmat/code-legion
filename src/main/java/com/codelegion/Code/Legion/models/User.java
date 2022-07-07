0990aa9a3bd8ed987f4d1d077a6efd643c0cbad4
kapich1192 <kapich1192@yandex.ru>
2022-07-07 13:46:26 +0300

    developing models

--- src/main/java/com/codelegion/Code/Legion/models/User.java
+++ src/main/java/com/codelegion/Code/Legion/models/User.java
@@ -11,13 +11,67 @@ public class User {
     private String nick;
     private String login;
     private String password;
+    private String status;
+    private String token;
+    private String githubLink;
+    /*=========================*/
+    /*peer to peer*/
+    private int peerPoints;
+    private int peerCoins;
+    private int codeReviewPoints;
+    private int articleCounter;
+    private int projectCounter;
+    private int helpCounter;
+    /*=========================*/
     /*game part fields*/
     private int level;
+    /*=========================*/
     /*basic skills*/
     private int htmlLevel;
     private int contentLevel;
     private int seoLevel;
     private int cssLevel;
     private int cLevel;
-    /**/
+    /*==========Medium Soft Skills===============*/
+    /*++++++++++Java+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++Python+++++++++++*/
+    /*Python syntax*/
+    /*Python core*/
+    /*Python projects*/
+    /*++++++++++CPP+++++++++++*/
+    /*CPP syntax*/
+    /*CPP core*/
+    /*CPP projects*/
+    /*++++++++++C+++++++++++*/
+    /*C syntax*/
+    /*C core*/
+    /*C projects*/
+    /*++++++++++JavaScript+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++Kotlin+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++C#+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++PHP+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++Swift+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*++++++++++Scala+++++++++++*/
+    /*Java syntax*/
+    /*Java core*/
+    /*Java projects*/
+    /*==========Hard Soft Skills===============*/
 }
