e33e6e96138a40c3342a7436a6d2c351e1804362
kapich1192 <kapich1192@yandex.ru>
2022-07-07 09:26:19 +0300

    first commit

---
+++ src/test/java/com/codelegion/Code/Legion/CodeLegionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelegion.Code.Legion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLegionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
