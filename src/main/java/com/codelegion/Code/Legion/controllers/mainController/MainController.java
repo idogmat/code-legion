54d07fbccb919af03bfe7a32c0226b73af8f9674
Dmitry <00912009@rambler.ru>
2022-07-07 17:42:07 +0300

    first draft

--- src/main/java/com/codelegion/Code/Legion/controllers/mainController/MainController.java
+++ src/main/java/com/codelegion/Code/Legion/controllers/mainController/MainController.java
@@ -1,4 +1,32 @@
 package com.codelegion.Code.Legion.controllers.mainController;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/general")
+    public String general(Model model) {
+        return "general";
+    }
+
+//    @PostMapping("/general")
+//    public void answerGeneral(Model model) {
+//
+//    }
+//    @DeleteMapping("/general")
+//    public void deleteGeneral(Model model) {
+//
+//    }
+//    @PutMapping("/general")
+//    public void correctAnswer(Model model) {
+//
+//    }
+//    @PatchMapping("/general")
+//    public void patchGeneral(Model model) {
+//
+//    }
 }
