6480fba4783f73335c9859c4a292de288e5bcf9f
kapich1192 <kapich1192@yandex.ru>
2022-07-07 17:16:18 +0300

    learMapController correct

--- src/main/java/com/codelegion/Code/Legion/controllers/lernMapController/LernMapController.java
+++ src/main/java/com/codelegion/Code/Legion/controllers/lernMapController/LernMapController.java
@@ -1,23 +1,31 @@
 package com.codelegion.Code.Legion.controllers.lernMapController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class LernMapController {
     @GetMapping("/learn")
-    public String learn() {
-        String result;
-        String tokenAvtorization = "NOTNULL"; //
-        if (tokenAvtorization == null) {
-            result = "login";
-        } else {
-            //todo полученеие данных из бд учебных материалов
-            result = "learn";
-        }
-        return result;
-    }
+    public String learn(Model model) {
 
+        return "learn.html";
+    }
+//    @PostMapping("/lern")
+//    public void answer(Model model) {
+//
+//    }
+//    @DeleteMapping("/lern")
+//    public void deleteAnswer(Model model) {
+//
+//    }
+//    @PutMapping("/lern")
+//    public void correctAnswer(Model model) {
+//
+//    }
     //todo получение массива на материалы
     //todo получение массива инструкций
     //todo получение массива на лекции
