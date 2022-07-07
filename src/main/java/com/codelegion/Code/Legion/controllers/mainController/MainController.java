460de5603d054e46a92c6b345fc9ea8d6cd06ef9
Dmitry <00912009@rambler.ru>
2022-07-07 17:43:28 +0300

    first draft

--- src/main/java/com/codelegion/Code/Legion/controllers/mainController/MainController.java
+++ src/main/java/com/codelegion/Code/Legion/controllers/mainController/MainController.java
@@ -3,7 +3,7 @@ package com.codelegion.Code.Legion.controllers.mainController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
